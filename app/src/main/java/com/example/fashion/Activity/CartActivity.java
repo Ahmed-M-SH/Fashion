b957c78ac0decdecfbbd132b7e0653756209548f
Ahmed-M-SH <homecr622@gmail.com>
2023-12-18 17:00:32 +0300

    الانتهاء من صفخه المنتج والبدء بصفحه my cart

--- app/src/main/java/com/example/fashion/Activity/CartActivity.java
+++ app/src/main/java/com/example/fashion/Activity/CartActivity.java
@@ -1,14 +1,16 @@
-package com.example.fashion;
+package com.example.fashion.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.fashion.R;
+
+public class CartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cart);
     }
 }
--- app/src/main/java/com/example/fashion/Activity/DetailActivity.java
+++ app/src/main/java/com/example/fashion/Activity/DetailActivity.java
@@ -1,4 +1,4 @@
-package com.example.fashion;
+package com.example.fashion.Activity;
 
 import android.app.Activity;
 
