a8f0ee9bd959a70897eb3c9f0ba9a6819b2d3ec1
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-12-20 05:21:24 +0300

    انتهاء قسم DetailActivity BackEnd والبدء بقسم CartActivity UI

--- app/src/main/java/com/example/fashion/Activity/DetailActivity.java
+++ app/src/main/java/com/example/fashion/Activity/DetailActivity.java
@@ -1,6 +1,78 @@
 package com.example.fashion.Activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.example.fashion.Domain.PopularDomain;
+import com.example.fashion.Helper.ManagmentCart;
+import com.example.fashion.R;
 
 public class DetailActivity extends Activity {
+    private Button addToCartBtn;
+    private TextView titleTxt,feeTxt,descriptionTxt,reviewTxt,scoreTxt;
+    private ImageView picItem,backBtn;
+    private PopularDomain object;
+    private int numberOrder=1;
+    private ManagmentCart managmentCart;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detaile);
+
+
+        managmentCart = new ManagmentCart(this);
+
+        backBtn=findViewById(R.id.backArrowBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        initView();
+        getBundle();
+    }
+
+    private void getBundle() {
+        object = (PopularDomain) getIntent().getSerializableExtra("object");
+        int drawableResourceId = this.getResources().getIdentifier(object.getPicUrl(),
+                "drawable",this.getPackageName());
+
+        Glide.with(this)
+                .load(drawableResourceId)
+                .into(picItem);
+
+        titleTxt.setText(object.getTitle());
+        descriptionTxt.setText(object.getDescription());
+        reviewTxt.setText(object.getReview()+"");
+        scoreTxt.setText(object.getScore()+"");
+        feeTxt.setText("$"+object.getPrice());
+
+        addToCartBtn.setOnClickListener(view -> {
+            object.setNumberinCart(numberOrder);
+            managmentCart.insertFood(object);
+        });
+    }
+
+    private void initView() {
+//        private TextView titleTxt,feeTxt,descriptionTxt,reviewTxt,scoreTxt;
+
+        addToCartBtn = findViewById(R.id.addToCartBtr);
+        titleTxt = findViewById(R.id.titleText);
+        feeTxt = findViewById(R.id.priceTxt);
+        descriptionTxt = findViewById(R.id.descriptionTxt);
+        reviewTxt = findViewById(R.id.reviewTex);
+        scoreTxt=findViewById(R.id.scoreTxt);
+        picItem = findViewById(R.id.itemPic);
+
+    }
 }
--- app/src/main/java/com/example/fashion/Activity/MainActivity.java
+++ app/src/main/java/com/example/fashion/Activity/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.fashion.Adapter.PopularListAdapter;
 import com.example.fashion.Domain.PopularDomain;
@@ -24,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         initRecyclerView();
+
+        bottom_navigation();
+
+
+    }
+
+    private void bottom_navigation() {
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+        LinearLayout cartBtn = findViewById(R.id.cartBtn);
+        homeBtn.setOnClickListener(view -> startActivity( new Intent(MainActivity.this,MainActivity.class)));
+        cartBtn.setOnClickListener(view -> startActivity( new Intent(MainActivity.this,CartActivity.class)));
     }
 
     private void initRecyclerView() {
@@ -31,19 +45,19 @@ public class MainActivity extends AppCompatActivity {
         item.add(new PopularDomain("slkdjflsadjflksjfd","jsdfsfkjksafdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj\n" +
                 "lksjdflksjdfffffffffffffffffffjkllffffffffffffffff\n" +
                 "lksadffffffffffffffffffffffffffffffffffffffff\n" +
-                "lsdkdddddddddddddddddddddddddddddddddddddddddddddd","pic1",15,20,4));
+                "lsdkdddddddddddddddddddddddddddddddddddddddddddddd","pic1",15,20,4152000));
         item.add(new PopularDomain("asdflkjfhhkjhhhhhhh","sdkkkkkkkkkkkkkkkkkkkkkkkkkkkkdd\n" +
                 "sdddddddddddddddddddddddddddddddddddddddddddddddddddd\n" +
                 "dfffffffffffffffffffffffffffffffffffffffffffffffffffff\n" +
-                "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbf\n","pic2",10,25,5));
+                "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbf\n","pic2",10,25,50025));
         item.add(new PopularDomain("sdlkfjlsdjflsakjdf","iiiiiiiiiiiiiiiiiiiiiiiiiisoadifoosdf\n" +
                 "allllllllllllllllllllllllllllllllllllllllksdlf\n" +
                 "laksdddddkkkdkjfkgjgggggggggggggggggg\n" +
                 "asdffffffffffffffffffffffffffffffffff\n" +
-                "dfffffffffffffffffffffffff","pic3",13,30,4.5));
+                "dfffffffffffffffffffffffff","pic3",13,30,412256.5));
         item.add(new PopularDomain("fffffffffsdfdd","asldlklklkkkkkkkkkkkkkkkkkkklsldfllkjlkdjjl\n" +
                 "alskdflksjfddddddddddddddddddddd\n" +
-                "sdddddddddddddddddddddddddddddddddddddddddddd\n","pic1",13,33,7.5));
+                "sdddddddddddddddddddddddddddddddddddddddddddd\n","pic1",13,33,7500.5));
         recyclerView =findViewById(R.id.view1);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
         adapterPupolar=new PopularListAdapter(item);
