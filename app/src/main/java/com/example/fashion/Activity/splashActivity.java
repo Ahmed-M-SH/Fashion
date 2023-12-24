aa9676b4c7dda300cc2c42ea5540387fac0b69a0
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-12-25 00:11:07 +0300

    add signupActivity.java and loginActivity.java and splashActivity.java and SharedPreferencesHelper.java for the project

---
+++ app/src/main/java/com/example/fashion/Activity/splashActivity.java
@@ -0,0 +1,59 @@
+package com.example.fashion.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fashion.Models.SharedPreferencesHelper;
+import com.example.fashion.R;
+
+public class splashActivity extends AppCompatActivity {
+    private Button startBtn;
+    private static final int SPLASH_TIME_OUT = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferencesHelper prefsHelper = new SharedPreferencesHelper(splashActivity.this);
+
+                // Check if the user is logged in
+                boolean isLoggedIn = !TextUtils.isEmpty(prefsHelper.getEmail()) && !TextUtils.isEmpty(prefsHelper.getPassword());
+
+                if (isLoggedIn) {
+                    // User is already logged in, navigate to the main activity
+                    Intent intent = new Intent(splashActivity.this, MainActivity.class); // Replace with your main activity
+                    startActivity(intent);
+                } else {
+                    // User is not logged in, navigate to the login activity
+                    Intent intent = new Intent(splashActivity.this, introActivity.class); // Replace with your login activity
+                    startActivity(intent);
+                }
+
+                finish(); // Close the splash activity
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+//        initView();
+
+
+    private void initView(){
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(splashActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
